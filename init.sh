de2c1af9f16a034e6a3e83fc8cfa453114b95b19
Youssef Mohammed El-Shabrawii <72987571+SHABRAWii@users.noreply.github.com>
2022-07-19 00:38:29 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+sudo apt install -y default-jdk
+sudo apt install -y default-jre
+sudo apt install -y python python3
+pip install pyinstaller
+sudo apt install -y build-essential
+sudo apt install -y g++
