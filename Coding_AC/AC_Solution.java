20b8afcc6930bc201db9d79a43c0d67e39dd150e
Youssef Mohammed El-Shabrawii <youssef.elshabrawii@gmail.com>
2022-08-13 19:41:39 +0200

    Update AC_Solution.java

--- Coding_AC/AC_Solution.java
+++ Coding_AC/AC_Solution.java
@@ -1,15 +1,9 @@
 package Coding_AC;
-import java.util.*;
 
 public class AC_Solution {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		// int[] storer = {10,85,62};
-		// System.out.println(storer[3]);
-		System.out.println(t + 1);
-        sc.close();
+
 	} 
 
 }
